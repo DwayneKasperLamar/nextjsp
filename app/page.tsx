2ac4dc1c51ef0a93e043c28b4768e9a8bc438629
Dwaynekasper <Dwaynekasperlamar@gmail.com>
2024-06-10 01:36:23 +0100

    Here RecentProjects was implimented

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,20 @@ import Hero from "@/Components/Hero";
 import { FloatingNav } from "@/Components/ui/FloatingNav";
 import Grid from "@/Components/Grid";
 import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/Components/RecentProjects"
 
 export default function Home() {
 	return (
 		<main
 			className="relative bg-black-100 flex justify-center items-center 
-    flex-col overflow-hidden mx-auto sm:px-10 px-5">
+			flex-col overflow-hidden mx-auto sm:px-10 px-5">
 			<div className="max-w-7xl w-full">
 				<FloatingNav
 					navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
 				/>
 				<Hero />
 				<Grid />
+				<RecentProjects/>
 			</div>
 		</main>
 	);
